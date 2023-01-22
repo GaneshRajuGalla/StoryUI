5adb28f85631e4da35b0f42c35b91519489f3bad
Ganesh Raju Galla <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-01-22 16:25:49 +0530

    Add files via upload

---
+++ Story/Story/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Story
+//
+//  Created by Ganesh Raju Galla on 22/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        StatusView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Story/Story/StoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryApp.swift
+//  Story
+//
+//  Created by Ganesh Raju Galla on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct StoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
